c7ed9abfcaf00d11f952a112d745a9f74f5acee1
Jake Hammond <jakehammond226@gmail.com>
2022-08-02 21:27:00 -0500

    first push

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+
+Console.WriteLine("Please enter in a zip code you would like to know the current tempterature of.");
+int zipCode = int.Parse(Console.ReadLine()); 
+
+var client = new HttpClient();
+
+var apiResponse = File.ReadAllText("appsettings.json");
+
+var apiKey = JObject.Parse(apiResponse).GetValue("weatherKey"); 
+
+string weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode},US&appid={apiKey}&units=imperial";
+
+var weatherJsonResponse = client.GetStringAsync(weatherURL).Result;    //Connects to Internet
+
+var weatherObject = JObject.Parse(weatherJsonResponse);
+
+double currentTemp = double.Parse(weatherObject["main"]["temp"].ToString());
+
+string currentLocation = weatherObject.GetValue("name").ToString(); 
+
+Console.WriteLine($"The current temperate at {currentLocation} with {zipCode} is {currentTemp}"); 
+
+
